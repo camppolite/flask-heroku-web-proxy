59a01af7ed5d9bec53f06320e403fe30a807c73c
camp <qianbeiyouli@gmail.com>
2018-12-14 16:08:57 +0800

    暂时只支持get方法的代理，无法处理js

--- nogfw/run.py
+++ nogfw/run.py
@@ -5,10 +5,12 @@ For example:
 http://localhost:8080/p/www.google.com
 """
 
-import re
-from flask import Flask, render_template, request, abort, Response, redirect,make_response, stream_with_context
+from flask import (
+    Flask, render_template, request, abort, Response, redirect,make_response, stream_with_context, url_for
+)
 import requests
 import logging
+import re
 
 app = Flask(__name__)
 
@@ -20,18 +22,27 @@ logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger("main.py")
 
 
-@app.route('/proxy', methods=["GET", "POST", "PUT", "DELETE"])
-def proxy_req():
+@app.route('/proxy/<path:url>', methods=["GET", "POST", "PUT", "DELETE"])
+def proxy(url):
     """根据输入的url，通过服务器获取url再返回给客户端"""
+    if not url:
+        return render_template('index.html')
+    print(url)
+    req = requests.get(url, stream=True)
+    return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
+
+
+@app.route('/no_js', methods=["GET", "POST"])
+def no_js():
+    """获取用户输入的url，重定向到代理网址"""
     if request.method == 'POST':
         url = request.form["url"]
         if not url:
             return render_template('index.html')
         print(url)
-        req = requests.get(url, stream=True)
-        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
+        return redirect(url_for('proxy', url=url))
 
-    return render_template('index.html')
+    return redirect(url_for('index'))
 
 
 @app.route('/')
