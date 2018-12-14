d58a16944c3c6e6a276069f14c35c79f0fd3eddc
camp <qianbeiyouli@gmail.com>
2018-12-14 14:52:55 +0800

    增加界面ui

--- nogfw/run.py
+++ nogfw/run.py
@@ -20,21 +20,23 @@ logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger("main.py")
 
 
-@app.route('/proxy/<path:url>', methods=["GET", "POST", "PUT", "DELETE"])
-def proxy(url):
-    """Fetches the specified URL and streams it out to the client.
-    If the request was referred by the proxy itself (e.g. this is an image fetch for
-    a previously proxied HTML page), then the original Referer is passed."""
-    url = re.sub('/proxy/', "", request.full_path)
-    print(url)
-    print(request.method)
-    req = requests.get(url, stream=True)
-    return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
+@app.route('/proxy', methods=["GET", "POST", "PUT", "DELETE"])
+def proxy_req():
+    """根据输入的url，通过服务器获取url再返回给客户端"""
+    if request.method == 'POST':
+        url = request.form["url"]
+        if not url:
+            return render_template('index.html')
+        print(url)
+        req = requests.get(url, stream=True)
+        return Response(stream_with_context(req.iter_content()), content_type=req.headers['content-type'])
+
+    return render_template('index.html')
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
